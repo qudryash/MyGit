9fcd10659862e9921196b9c6cf0d918cd245f5d2
qudryash <qudryashovav@yandex.ru>
2024-01-11 21:53:03 +0700

    Initial Commit

---
+++ MyGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyGit
+//
+//  Created by Kudryashov Andrey on 11.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
